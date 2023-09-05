6b5272e058e62ad0be5c213a5ce0c98156433476
SebastianHeiberg <seba3673@stud.kea.dk>
2023-09-05 21:23:31 +0200

    patch working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -11,62 +11,73 @@ app.listen(PORT, (err) => {
   console.log("server is running on port", PORT);
 });
 
-app.get("/mountains",  async (req, res) => {
+app.get("/mountains", async (req, res) => {
   
-  fs.readFile(`data/mountains.json`, "utf-8", (err, data) => {
-    if (err) {
-      res.status(500);
-      return res.json(`Error reading file`);
-    }
+  //read all mountains helper function
+  const mountainslist = await readMountains();
 
-    const mountains = JSON.parse(data).mountains
-    res.status(200);
-    return res.send({ data: mountains});
+  res.status(200);
+  return res.send({ data: mountainslist });
 
-  });
-})
+});
 
-
-app.get("/mountains/:id", (req, res) => {
+app.get("/mountains/:id", async (req, res) => {
   const pathVarMountainId = Number(req.params.id);
 
   if (!pathVarMountainId) {
     res.send({ error: "the mountain id must be a number" });
   }
 
-  fs.readFile(`data/mountains.json`, "utf-8", (err, data) => {
-    if (err) {
-      res.status(500);
-      return res.json(`Error reading file`);
-    }
-    
-    const mountains = JSON.parse(data).mountains
-    const foundMountain = mountains.find(
-      (mountain) => mountain.id === Number(req.params.id)
-    );
+  //read all mountains helper function
+  const mountains = await readMountains();
 
-    res.status(200);
-    return res.send( { data: foundMountain});
+  //find the one chosen mountain
+  const foundMountain = mountains.mountains.find(
+    (mountain) => mountain.id === Number(req.params.id)
+  );
 
+  res.status(200);
+  return res.send({ data: foundMountain });
   });
 
-});
 
-//post
 app.post("/mountains", (req, res) => {
-  
   const mountains = [];
 
- 
-  
-  console.log(req.body.name)
-  res.send(req.body)
-})
-
+  console.log(req.body.name);
+  res.send(req.body);
+});
 
 //patch
+app.patch("/mountains/:id", async (req, res) => {
+  const pathVarMountainId = Number(req.params.id);
 
-//delete
+  if (!pathVarMountainId) {
+    res.send({ error: "the mountain id must be a number" });
+  }
+
+  //read all mountains helper function
+  const mountains = await readMountains();
+
+  //find the one chosen
+  const foundMountain = mountains.mountains.find(
+  (mountain) => mountain.id === Number(req.params.id)
+  );
+  
+  //update the mountain
+  if(req.body.height){ foundMountain.height = req.body.height;}
+  if(req.body.name){ foundMountain.name = req.body.name }
+  if(req.body.continent) { foundMountain.continent = req.body.continent }   
+
+  //save the mountain
+  saveMountains(mountains);
+
+    res.status(200);
+    return res.send({ data: foundMountain });
+  });
+
+
+
 app.delete("/mountains/:id", (req, res) => {
   const pathVarMountainId = Number(req.params.id);
 
@@ -80,31 +91,43 @@ app.delete("/mountains/:id", (req, res) => {
       res.status(500);
       return res.json(`Error reading file`);
     }
-    
+
     //filter the chosen mountain out
-    const loadMountains = JSON.parse(data).mountains
+    const loadMountains = JSON.parse(data).mountains;
     const listRemainingMountains = loadMountains.filter(
       (mountain) => mountain.id !== Number(req.params.id)
     );
-    const saveMountains = {mountains: listRemainingMountains}
+    const mountains = { mountains: listRemainingMountains };
 
-      //Saves all the mountains
-    fs.writeFile(`data/mountains.json`, JSON.stringify(saveMountains), (err) => {
-
-      if (err) {
-      res.status(500);
-      return res.json(`Error writing file`);
-    }}
-    );
+    //Saves all the mountains helper function
+    saveMountains(mountains);
 
     //return all OK, and a message
     res.status(200);
-    return res.send( { data: 'Deleted Mountain with id: ' + pathVarMountainId});
-
+    return res.send({ data: "Deleted Mountain with id: " + pathVarMountainId });
   });
-
 });
 
+function saveMountains(mountains){
+  fs.writeFile(
+    `data/mountains.json`,
+    JSON.stringify(mountains),
+    (err) => {
+      if (err) {
+        res.status(500);
+        return res.json(`Error writing file`);
+      }
+    }
+  );
+}
 
+async function readMountains() {
+  
+  const mountains = await fs.readFile(`data/mountains.json`, "utf-8", (err) => {
+    if (err) {
+      console.log('failed to read file');
+    }});
 
-
+  const mountainsJson = JSON.parse(mountains);
+  return mountainsJson; 
+}
