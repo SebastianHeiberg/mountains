3acb16e65ee12401cf866dbccdf53be8f2ea9fcf
SebastianHeiberg <seba3673@stud.kea.dk>
2023-09-06 11:18:54 +0200

    Working post and a counter

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ app.listen(PORT, (err) => {
   console.log("server is running on port", PORT);
 });
 
+
 app.get("/mountains", async (req, res) => {
   
-  //read all mountains helper function
+  //read all mountains
   const mountainslist = await readMountains();
 
   res.status(200);
   return res.send({ data: mountainslist });
 
 });
+
 
 app.get("/mountains/:id", async (req, res) => {
   const pathVarMountainId = Number(req.params.id);
@@ -28,7 +30,7 @@ app.get("/mountains/:id", async (req, res) => {
     res.send({ error: "the mountain id must be a number" });
   }
 
-  //read all mountains helper function
+  //read all mountains
   const mountains = await readMountains();
 
   //find the one chosen mountain
@@ -41,14 +43,42 @@ app.get("/mountains/:id", async (req, res) => {
   });
 
 
-app.post("/mountains", (req, res) => {
-  const mountains = [];
+app.post("/mountains", async (req, res) => {
 
-  console.log(req.body.name);
-  res.send(req.body);
+  const name = req.body.name
+  const height = req.body.height
+  const continent = req.body.continent
+
+  if (!name || !height || !continent){
+    res.status(400)
+    res.send({message: 'Info is missing'})
+  } else {
+
+  //make a new mountain with a uniqe id
+  const idCounter = await readIdCounter();
+
+  const mountain = {name: name, height: height, continent: continent, id: idCounter.counter};
+
+  //read all mountains
+  const mountains = await readMountains();
+
+  //push the new one
+  mountains.mountains.push(mountain);
+
+  //save mountains
+  saveMountains(mountains);
+
+  //increment id
+  incrementIdCounter(idCounter);
+
+  //return new mountain
+  res.status(200);
+  res.send({data: mountain});
+}
+
 });
 
-//patch
+
 app.patch("/mountains/:id", async (req, res) => {
   const pathVarMountainId = Number(req.params.id);
 
@@ -56,7 +86,7 @@ app.patch("/mountains/:id", async (req, res) => {
     res.send({ error: "the mountain id must be a number" });
   }
 
-  //read all mountains helper function
+  //read all mountains
   const mountains = await readMountains();
 
   //find the one chosen
@@ -65,17 +95,22 @@ app.patch("/mountains/:id", async (req, res) => {
   );
   
   //update the mountain
-  if(req.body.height){ foundMountain.height = req.body.height;}
-  if(req.body.name){ foundMountain.name = req.body.name }
-  if(req.body.continent) { foundMountain.continent = req.body.continent }   
+  if(req.body.height){ 
+    foundMountain.height = req.body.height
+  };
+  if(req.body.name){ 
+    foundMountain.name = req.body.name 
+  };
+  if(req.body.continent) { 
+    foundMountain.continent = req.body.continent 
+  }   
 
   //save the mountain
   saveMountains(mountains);
 
-    res.status(200);
-    return res.send({ data: foundMountain });
+  res.status(200);
+  return res.send({ data: foundMountain });
   });
-
 
 
 app.delete("/mountains/:id", (req, res) => {
@@ -99,20 +134,16 @@ app.delete("/mountains/:id", (req, res) => {
     );
     const mountains = { mountains: listRemainingMountains };
 
-    //Saves all the mountains helper function
+    //Saves all the mountains
     saveMountains(mountains);
 
-    //return all OK, and a message
     res.status(200);
     return res.send({ data: "Deleted Mountain with id: " + pathVarMountainId });
   });
 });
 
 function saveMountains(mountains){
-  fs.writeFile(
-    `data/mountains.json`,
-    JSON.stringify(mountains),
-    (err) => {
+  fs.writeFile(`data/mountains.json`, JSON.stringify(mountains),(err) => {
       if (err) {
         res.status(500);
         return res.json(`Error writing file`);
@@ -130,4 +161,29 @@ async function readMountains() {
 
   const mountainsJson = JSON.parse(mountains);
   return mountainsJson; 
+}
+
+
+function incrementIdCounter(idCounter){
+
+  idCounter.counter ++;
+
+  fs.writeFile(`data/idCounter.json`, JSON.stringify(idCounter),(err) => {
+      if (err) {
+        res.status(500);
+        return res.json(`Error writing file`);
+      }
+    }
+  );
+}
+
+async function readIdCounter() {
+  
+  const idCounter = await fs.readFile(`data/idCounter.json`, "utf-8", (err) => {
+    if (err) {
+      console.log('failed to read file');
+    }});
+
+  const idJson = JSON.parse(idCounter);
+  return idJson; 
 }
